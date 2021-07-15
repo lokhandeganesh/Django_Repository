fd2225653d12114ddf4f8490fe009d3f887dc820
lokhandeganesh <lokhandeganesh8@gmail.com>
2021-07-16 00:42:17 +0530

    Lecture13

--- pocra_dashboard/urls.py
+++ pocra_dashboard/urls.py
@@ -19,5 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('application.urls'))
+    path('',include('travello.urls')),
+    # path('',include('application.urls')),
+
 ]
---
+++ travello/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+# From all modules import views#
+from . import views
+
+# set url patern for home/index page#
+urlpatterns = [
+    path('',views.index,name='index'),
+    
+]
